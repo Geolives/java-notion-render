860b12a1c9efc73471cea2f32f4b80503520bd8b
Christophe Brasseur <christophe.brasseur@geolives.com>
2024-04-25 17:52:11 +0200

    Test

--- lib-notion-java-sdk/src/main/java/com/geolives/NotionClient.java
+++ lib-notion-java-sdk/src/main/java/com/geolives/NotionClient.java
@@ -29,6 +29,7 @@ public class NotionClient {
     private static final RateLimiter rateLimiter = RateLimiter.create(100.0 / 60.0);
     private final OkHttpClient httpClient;
     private final ObjectMapper mapper;
+
     private final NotionUrlBuilder urlBuilder;
     private final NotionRequestBuilder requestBuilder;
 
